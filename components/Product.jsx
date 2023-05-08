b9c16082feb440058b7eb9009c4be1b97bf46383
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-05-08 17:17:57 +0300

    photo name change

--- components/Product.jsx
+++ components/Product.jsx
@@ -7,12 +7,12 @@ const Product = () => {
     <section id="product" className="max-w-[1640px] mx-auto p-4 py-16">
       <Headline title="Products" />
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 px-20 py-16">
-        <Card image="/assets/furniture1.avif" title="Furniture 1" />
-        <Card image="/assets/furniture2.avif" title="Furniture 2" />
-        <Card image="/assets/furniture3.avif" title="Furniture 3" />
-        <Card image="/assets/furniture4.avif" title="Furniture 4" />
-        <Card image="/assets/furniture5.avif" title="Furniture 5" />
-        <Card image="/assets/furniture6.avif" title="Furniture 6" />
+        <Card image="/assets/furniture1.avif" title="furniture 1" />
+        <Card image="/assets/furniture2.avif" title="furniture 2" />
+        <Card image="/assets/furniture3.avif" title="furniture 3" />
+        <Card image="/assets/furniture4.avif" title="furniture 4" />
+        <Card image="/assets/furniture5.avif" title="furniture 5" />
+        <Card image="/assets/furniture6.avif" title="furniture 6" />
       </div>
     </section>
   );
